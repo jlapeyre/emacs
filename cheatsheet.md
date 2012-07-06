238b6aa88ed6266a4823d281b4f9502273c163b8
Jens Haase <je.haase@googlemail.com>
2012-07-06 09:09:29 +0200

    Added magit and markdown. Started to write a small cheat sheet.

---
+++ cheatsheet.md
@@ -0,0 +1,11 @@
+# Emacs cheat sheet
+
+## Motion
+
+* `M-m` First Char
+
+## Copy & Paste
+
+* `C-w` Cut
+* `M-w` Copy
+* `C-y` Paste (yank)
--- customizations/my-modes.el
+++ customizations/my-modes.el
@@ -1,3 +1,9 @@
+; Git
+(require 'magit)
+(autoload 'magit-status "magit" nil t)
+(autoload 'mo-git-blame-file "mo-git-blame" nil t)
+(autoload 'mo-git-blame-current "mo-git-blame" nil t)
+
 ; Scala
 (require 'scala-mode)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
@@ -5,6 +11,13 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/ensime_2.9.2-RC1-0.9.3.RC4/elisp/")
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+; Markdown
+(autoload 'markdown-mode "markdown-mode.el"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mkd$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 
 
 ; Enable fill column indicator
